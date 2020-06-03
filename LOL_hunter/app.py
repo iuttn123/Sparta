4debcc52a1f849d1a04122d4aaca7ad55f893a24
iuttn123 <49703321+iuttn123@users.noreply.github.com>
2020-06-03 13:18:41 +0900

    op.gg를 통해서 데이터 확인함

--- LOL_hunter/app.py
+++ LOL_hunter/app.py
@@ -1,16 +1,50 @@
 import requests
 from bs4 import BeautifulSoup
+from flask import Flask, render_template, request,jsonify
 
+
+app = Flask(__name__)
 
 # headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-# data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303',headers=headers)
+# data = requests.get('https://www.op.gg/summoner/userName=%ED%95%AB%20%EB%B6%80',headers=headers)
 
-# soup = BeautifulSoup(data,'html.parser')
+
+# data를 text형태로 바꿔야 한다.
+
+# print(data) -> 200 ok 반응이 나온다
+# print(data.text) -> 크롤링 해온다
+
+# # request에 대한 기본 이해도
+# naver = 'https://www.naver.com/'
+# response = requests.get(url=naver)
+# print(response.text)
+
+
+
+# soup = BeautifulSoup(data.text,'html.parser')
 # print(soup)
 
+@app.route('/')
+def home():
+    return render_template("Main.html")
+
+@app.route('/test', methods=['GET'])
+def test_get():
+   ID_Search = request.args.get('ID_Search')
+   headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+   data = requests.get('https://www.op.gg/summoner/userName='+ID_Search,headers=headers)   
+   soup = BeautifulSoup(data.text,'html.parser')
 
 
-# request에 대한 기본 이해도
-naver = 'https://www.naver.com/'
-response = requests.get(url=naver)
-print(response.text)
+   mydivs = soup.findAll("div", {"class": "SummonerNotFoundLayout"})
+   if(len(mydivs)==0): 
+        result=True
+   else:
+        result=False
+
+   # return print(result) 
+   return jsonify({'result':result}) 
+
+
+if __name__ == '__main__':  
+   app.run('localhost',port=5000,debug=True)
